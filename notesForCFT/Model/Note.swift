9adf15ba3c81c042ca81bad9fd135f59e0ed6f4a
Vadim Igdisanov <Igidsanov@t-dev.pro>
2023-01-10 17:15:27 +0500

    добавил модель и презентер

---
+++ notesForCFT/Model/Note.swift
@@ -0,0 +1,11 @@
+//
+//  Note.swift
+//  notesForCFT
+//
+//  Created by Vadim Igdisanov on 10.01.2023.
+//
+
+struct Note {
+    let name: String
+    let descriptions: String
+}
---
+++ notesForCFT/Presenter/NotesPresenter.swift
@@ -0,0 +1,10 @@
+//
+//  NotesPresenter.swift
+//  notesForCFT
+//
+//  Created by Vadim Igdisanov on 10.01.2023.
+//
+
+class NotesPresenter {
+    
+}
